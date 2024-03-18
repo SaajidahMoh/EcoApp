e32397343175eccd8ff513747bf559ca9ab21267
SaajidahMo <71775470+SaajidahMo@users.noreply.github.com>
2024-03-18 09:36:11 +0000

    Initial Commit

---
+++ EcoApp/EcoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoAppApp.swift
+//  EcoApp
+//
+//  Created by Saajidah Mohamed on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
