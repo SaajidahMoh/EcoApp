92ebc817e8f0b039a4896b073265e0a578a7bba8
SaajidahMo <71775470+SaajidahMo@users.noreply.github.com>
2024-04-24 22:46:06 +0100

    iphone pro max

--- EcoApp/Recipe/RecipeSearchView.swift
+++ EcoApp/Recipe/RecipeSearchView.swift
@@ -21,112 +21,113 @@ struct RecipeSearchView: View {
     let url: String
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false){
-            
-            //  "Failed to produce diagnostic for expression; please submit a bug report (https://swift.org/contributing/#reporting-bugs)"
-            Rectangle()
-                .fill(Color.clear)
-                .frame(height: 90)
-            
-            VStack(alignment: .center, spacing : 0){
-                let photoURL = URL(string: image)
-                AsyncImage(url: photoURL) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .clipped()
-                } placeholder: {
-                    ProgressView()
-                }
-
-                Group {
-                    Text("\(title)")
-                        .font(.system(.title))
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color (.systemGreen))
-                        .padding(.top, 10)
-                }
-                .padding(.horizontal, 24)
-                .padding(.vertical, 12)
+            ScrollView(.vertical, showsIndicators: false){
                 
-                VStack(alignment: .leading){
-                    HStack {
-                        if totalTime > 0.01 {
-                            Image(systemName: "clock.arrow.circlepath")
-                            Text(": \(Int(round(totalTime))) mins")
-                        }
+                //  "Failed to produce diagnostic for expression; please submit a bug report (https://swift.org/contributing/#reporting-bugs)"
+                Rectangle()
+                    .fill(Color.clear)
+                    .frame(height: 90)
+                
+                VStack(alignment: .center, spacing : 0){
+                    let photoURL = URL(string: image)
+                    AsyncImage(url: photoURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipped()
+                    } placeholder: {
+                        ProgressView()
                     }
-                    HStack {
-                        Image(systemName: "globe")
-                        ForEach(cuisineTypes, id: \.self) { cuisineType in
-                            let globe = cuisineType.capitalized
-                            Text(": \(globe)")
-                            
-                            Spacer()
-                            
-                            Button(action: shareRecipe
-                            ){
-                                Image(systemName: "square.and.arrow.up")
-                                    .resizable()
-                                    .frame(width: 21, height: 30)
-                                    .padding(10)
-                                //    .bold()
+                    
+                    Group {
+                        Text("\(title)")
+                            .font(.system(.title))
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color (.systemGreen))
+                            .padding(.top, 10)
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.vertical, 12)
+                    
+                    VStack(alignment: .leading){
+                        HStack {
+                            if totalTime > 0.01 {
+                                Image(systemName: "clock.arrow.circlepath")
+                                Text(": \(Int(round(totalTime))) mins")
                             }
                         }
-                    }
-                    Text("Ingredients")
-                        .fontWeight(.bold)
-                        .font(.system(.title2))
-                    
-                    ForEach(ingredients, id: \.self) { ingredient in
-                        VStack(alignment: .leading, spacing:6){
-                            Spacer()
-                       
-                        //HStack {
-                         //   Image(systemName: "star.fill")
-                        //        .foregroundColor(.green)
-                         //       .frame(width:10, height: 10)
-                            Text(ingredient)
-                               // .font(.subheadline)
-                                .font(.system(size: 16))
-                            Divider()
-                        }
-                    }
-                    
-                } .padding(.leading, 8)
-                    .padding(.trailing, 8)
-                
-                VStack(alignment: .center, spacing: 0){
-                    Spacer()
-                    Text("")
-                    Spacer()
-                    Text ("")
-                    Spacer()
-                    Link(destination: URL(string: url)!) {
                         HStack {
-                            Image(systemName: "link")
-                            Text("View Recipe")
-                                .frame(width: 110, height: 40)
-                                .multilineTextAlignment(.center)
-                                .font(.system(.title3))
-                        }  .multilineTextAlignment(.center)
+                            Image(systemName: "globe")
+                            ForEach(cuisineTypes, id: \.self) { cuisineType in
+                                let globe = cuisineType.capitalized
+                                Text(": \(globe)")
+                                
+                                Spacer()
+                                
+                                Button(action: shareRecipe
+                                ){
+                                    Image(systemName: "square.and.arrow.up")
+                                        .resizable()
+                                        .frame(width: 21, height: 30)
+                                        .padding(10)
+                                    //    .bold()
+                                }
+                            }
+                        }
+                        Text("Ingredients")
+                            .fontWeight(.bold)
+                            .font(.system(.title2))
+                        
+                        ForEach(ingredients, id: \.self) { ingredient in
+                            VStack(alignment: .leading, spacing:6){
+                                Spacer()
+                                
+                                //HStack {
+                                //   Image(systemName: "star.fill")
+                                //        .foregroundColor(.green)
+                                //       .frame(width:10, height: 10)
+                                Text(ingredient)
+                                // .font(.subheadline)
+                                    .font(.system(size: 16))
+                                Divider()
+                            }
+                        }
+                        
+                    } .padding(.leading, 8)
+                        .padding(.trailing, 8)
+                    
+                    VStack(alignment: .center, spacing: 0){
+                        Spacer()
+                        Text("")
+                        Spacer()
+                        Text ("")
+                        Spacer()
+                        Link(destination: URL(string: url)!) {
+                            HStack {
+                                Image(systemName: "link")
+                                Text("View Recipe")
+                                    .frame(width: 150, height: 40)
+                                    .multilineTextAlignment(.center)
+                                    .font(.system(.title3))
+                            }  .multilineTextAlignment(.center)
+                        }
+                        .padding()
+                        .buttonStyle(.borderedProminent)
+                        
+                        .multilineTextAlignment(.center)
+                        
                     }
-                    .padding()
-                    .buttonStyle(.borderedProminent)
                     
-                    .multilineTextAlignment(.center)
-                    
-                }
-                
-            } .padding(.leading)
-                .padding(.trailing)
+                } .padding(.leading)
+                    .padding(.trailing)
+            }
+            .edgesIgnoringSafeArea(.top)
+            
+            
         }
-        .edgesIgnoringSafeArea(.top)
-        
-        
-        
-    }
+      
+    
     
     func shareRecipe() {
         // https://chat.openai.com/share/3b6d71c7-ab4b-4458-9a07-c11a5bf6a363
