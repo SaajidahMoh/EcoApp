9b0a925f6915b4760711b34e1981b70543ac00d8
SaajidahMo <71775470+SaajidahMo@users.noreply.github.com>
2024-04-26 22:34:16 +0100

    main

--- EcoApp/Recipe/recipeSearch.swift
+++ EcoApp/Recipe/recipeSearch.swift
@@ -8,13 +8,60 @@
 
 import SwiftUI
 
+
+/*
+ code adapted from:
+ https://xavier7t.com/swiftui-list-with-sort-options
+ */
+enum SortOption {
+    case quickest, longest
+}
+
+
 struct recipeSearch: View {
     @State private var searchText: String = ""
     @State private var recipes: [Hit] = []
     @State private var isSaved : Bool = false
+    @State private var isAscending = false
+    @State private var selectedTheme = "Select Option"
+    var UniqueCuisineTypes: [String] = ["Select Option","american", "asian", "british", "caribbean", "central europe", "chinese",  "eastern europe", "french", "greek",  "indian", "italian", "japanese", "korean", "kosher", "mediterranean", "mexican", "middle eastern", "nordic", "south american", "south east asian", "world" ]
+    
+    
+    /*
+     code adapted from:
+     https://xavier7t.com/swiftui-list-with-sort-options
+     */
+     @State private var sortOption: SortOption = .quickest
+     var sortedTasks: [Hit] {
+         var filteredData = [Hit]()
+            if !selectedTheme.isEmpty {
+                filteredData = recipes.filter { $0.recipe.cuisineType.contains(selectedTheme) }
+            }; if selectedTheme == "Select Option"{
+                 filteredData = recipes
+            }
+         switch sortOption {
+         case .quickest:
+             return filteredData.sorted { $0.recipe.totalTime < $1.recipe.totalTime }
+         case .longest:
+             return filteredData.sorted { $0.recipe.totalTime > $1.recipe.totalTime }
+         }
+     }
     
     var body: some View {
         VStack {
+            HStack {
+                Picker("Appearance", selection: $selectedTheme) {
+                    ForEach(UniqueCuisineTypes, id: \.self) { cuisineType in
+                        Text(cuisineType).tag(cuisineType)
+                    }
+                }.pickerStyle(.menu)
+                
+                Picker("Sort By", selection: $sortOption) {
+                    Text("Shortest Time").tag(SortOption.quickest)
+                    Text("Longest Time").tag(SortOption.longest)
+                }.pickerStyle(.menu)
+            }
+            
             /** Button() {
                 
             } label: {
@@ -22,7 +69,7 @@ struct recipeSearch: View {
             } */
             
             NavigationView {
-                List(recipes, id: \.recipe.url) { hit in
+                List(sortedTasks, id: \.recipe.url) { hit in
                     NavigationLink(destination: RecipeSearchView(title: hit.recipe.label, ingredients: hit.recipe.ingredientLines, cuisineTypes: hit.recipe.cuisineType, image: hit.recipe.image, totalTime: hit.recipe.totalTime, url: hit.recipe.url)) {
                         RecipeCardView(hit: hit)
                             .padding(.vertical, 2)
@@ -60,7 +107,6 @@ struct recipeSearch: View {
             }
         }
     }
-
     
 }
 
